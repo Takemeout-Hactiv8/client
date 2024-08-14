4d044f82f427fde6d8361238653a800f1ea59dfb
zahara120 <zaharaa120@gmail.com>
2024-08-14 15:19:16 +0700

    feat: change socket to production server

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
+const socket = io('https://take-me-out.vivic.site/', {
     auth: (cb) => {
         cb({
             username: localStorage.username,
